644f07feb92630d49ea31ef03e8d86a47c115605
Paul <mainguy@mail.ru>
2020-10-03 17:54:15 +0300

    Update Index.html
    * Added sidebar

---
+++ assets/js/toggleSidebar.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+  document.getElementById('sidebar').classList.toggle('show');
+}
+
+function openSubItem() {
+  document.getElementById('sub-menu').classList.toggle('open');
+
+}
