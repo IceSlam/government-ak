22b548be02badd3f0267f16169218e948f2e008d
IceSlam <mainguy@mail.ru>
2020-10-06 00:00:24 +0300

    Completed News block on Index page

---
+++ assets/assets/js/toggleSidebar.js
@@ -0,0 +1,7 @@
+function toggleSidebar() {
+  document.getElementById('sidebar').classList.toggle('show');
+}
+
+function openSubItem() {
+  document.getElementById('sub-menu').classList.toggle('open')
+}
