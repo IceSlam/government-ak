17cf758ad9fcd8c5e275f9cf467b4995578f3b50
IceSlam <mainguy@mail.ru>
2020-10-06 18:55:34 +0300

    Updated project structure

--- assets/assets/js/toggleSidebar.js
+++
@@ -1,7 +0,0 @@
-function toggleSidebar() {
-  document.getElementById('sidebar').classList.toggle('show');
-}
-
-function openSubItem() {
-  document.getElementById('sub-menu').classList.toggle('open')
-}
